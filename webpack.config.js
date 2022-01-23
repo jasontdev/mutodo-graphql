573f6c132e31ec5f85d24d98503309f2f17d7b75
Jason Telfer <jason@jasont.dev>
2022-01-23 21:21:33 +1000

    Starter files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const NodemonPlugin = require("nodemon-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  target: "node",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "server.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new NodemonPlugin()],
+};
