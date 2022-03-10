9721aadce3486e3a26013949f5a1d4894f0ba109
Jason Telfer <jason@jasont.dev>
2022-03-10 18:48:25 +1000

    Initial setup of webpack, ts, eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Welcome to Node and TypeScript');
+interface User {
+  id: number;
+  name: string;
+}
+
+function printUser(user: User) {
+  console.log(`${user.name}`);
+}
+
+function main() {
+  const user: User = {
+    id: 0,
+    name: "Alfred",
+  };
+
+  printUser(user);
+}
+
+main();
