12b7c01986f791eeb2e5dc6695f02c54f96bd4db
Jason Telfer <jason@jasont.dev>
2022-03-25 18:29:27 +1000

    Added secret loading from .env or /run/secrets

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "graphql";
+import dotenv from "dotenv";
+import loadSecret from "./secrets";
 
 const schema = buildSchema(`
     type Query {
@@ -14,14 +16,28 @@ const root = {
   },
 };
 
-const app = express();
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
+(async () => {
+  dotenv.config();
+  try {
+    if (process.env.NODE_ENV) {
+      const jwtSecret = await loadSecret("jwt-secret", process.env.NODE_ENV);
+      console.log(`jwt-secret: ${jwtSecret}`);
+    } else {
+      console.log("Could not load jwt-secret");
+    }
+  } catch (error) {
+    console.log(`${error}`);
+  }
 
-app.listen(4100);
+  const app = express();
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      rootValue: root,
+      graphiql: true,
+    })
+  );
+
+  app.listen(4100);
+})();
