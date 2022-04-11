ae51c5527cc012cc8436b5e41a37364d469d2927
Jason T <jason@jasont.dev>
2022-04-11 21:55:52 +1000

    Created a wrapper for DynamoDBClient provided clients while keeping credentials private

---
+++ src/DatabaseClient.ts
@@ -0,0 +1,31 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+export type DatabaseClientConfig = {
+  region: string;
+  accessKeyId: string;
+  secretAccessKey: string;
+};
+
+class DatabaseClient {
+  #region: string;
+  #accessKeyId: string;
+  #secretAccessKey: string;
+
+  constructor({ region, accessKeyId, secretAccessKey }: DatabaseClientConfig) {
+    this.#region = region;
+    this.#accessKeyId = accessKeyId;
+    this.#secretAccessKey = secretAccessKey;
+  }
+
+  get get() {
+    return new DynamoDBClient({
+      region: this.#region,
+      credentials: {
+        accessKeyId: this.#accessKeyId,
+        secretAccessKey: this.#secretAccessKey,
+      },
+    });
+  }
+}
+
+export default DatabaseClient;
