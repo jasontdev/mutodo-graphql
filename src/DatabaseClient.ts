6e836f0363324c224f74a20e10297420ee1f2eff
Jason T <jason@jasont.dev>
2022-04-16 18:41:52 +1000

    userRepository now more easily tested. Started to build testing infrastructure

--- src/DatabaseClient.ts
+++ src/DatabaseClient.ts
@@ -2,16 +2,26 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export type DatabaseClientConfig = {
   region: string;
+  endpoint?: string;
   accessKeyId: string;
   secretAccessKey: string;
 };
 
 class DatabaseClient {
+  #endpoint: string | undefined;
   #region: string;
   #accessKeyId: string;
   #secretAccessKey: string;
 
-  constructor({ region, accessKeyId, secretAccessKey }: DatabaseClientConfig) {
+  constructor({
+    endpoint,
+    region,
+    accessKeyId,
+    secretAccessKey,
+  }: DatabaseClientConfig) {
+    if (endpoint) {
+      this.#endpoint = endpoint;
+    }
     this.#region = region;
     this.#accessKeyId = accessKeyId;
     this.#secretAccessKey = secretAccessKey;
@@ -19,6 +29,7 @@ class DatabaseClient {
 
   get(): DynamoDBClient {
     return new DynamoDBClient({
+      endpoint: this.#endpoint,
       region: this.#region,
       credentials: {
         accessKeyId: this.#accessKeyId,
---
+++ src/__tests__/user-repository.test.ts
@@ -0,0 +1,52 @@
+import {
+  CreateTableCommand,
+  ListTablesCommand,
+} from "@aws-sdk/client-dynamodb";
+import DatabaseClient from "../DatabaseClient";
+import { userRepository } from "../user-repository";
+
+beforeAll(async () => {
+  global.databaseClient = new DatabaseClient({
+    endpoint: "http://localhost:8000",
+    region: "ap-southeast-2",
+    accessKeyId: "abcd1234",
+    secretAccessKey: "abcd1234",
+  });
+
+  const list = await databaseClient.get().send(
+    new ListTablesCommand({
+      Limit: 1,
+    })
+  );
+
+  if (list.TableNames) {
+    if (list.TableNames[0] !== "mutodo") {
+      console.log("Creating table...");
+      databaseClient.get().send(
+        new CreateTableCommand({
+          TableName: "mutodo",
+          KeySchema: [
+            { AttributeName: "id", KeyType: "HASH" },
+            { AttributeName: "sort_key", KeyType: "RANGE" },
+          ],
+          AttributeDefinitions: [
+            { AttributeName: "id", AttributeType: "S" },
+            { AttributeName: "sort_key", AttributeType: "S" },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+        })
+      );
+    }
+  }
+});
+
+test("create a new user", async () => {
+  const authorizedUser = { sub: "abcd1234" };
+  const data = await userRepository.save({ name: "Alfred" }, authorizedUser);
+  if (data.Attributes) {
+    expect(data.Attributes.id).toBe("User_" + authorizedUser.sub);
+  }
+});
