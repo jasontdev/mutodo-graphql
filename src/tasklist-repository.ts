69d38069b43748aee0107b90f29c3b5a2141b870
Jason T <jason@jasont.dev>
2022-04-18 12:39:34 +1000

    Changed task-repository.ts exports to export {}

--- src/tasklist-repository.ts
+++ src/tasklist-repository.ts
@@ -2,38 +2,36 @@ import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import { randomUUID } from "crypto";
 import { AuthorizedUser, Tasklist } from "./types";
 
-const tasklistRepository = {
-  create: async function (tasklist: Tasklist, authorizedUser: AuthorizedUser) {
-    const uuid = randomUUID();
-    const ddbClient = databaseClient.get();
-    const documentClient = DynamoDBDocument.from(ddbClient);
+async function create(tasklist: Tasklist, authorizedUser: AuthorizedUser) {
+  const uuid = randomUUID();
+  const ddbClient = databaseClient.get();
+  const documentClient = DynamoDBDocument.from(ddbClient);
 
-    try {
-      await documentClient.put({
-        TableName: "mutodo",
-        Item: {
-          id: `tasklist_${uuid}`,
-          sort_key: `user_${authorizedUser.sub}`,
-          name: tasklist.name,
-        },
-      });
+  try {
+    await documentClient.put({
+      TableName: "mutodo",
+      Item: {
+        id: `tasklist_${uuid}`,
+        sort_key: `user_${authorizedUser.sub}`,
+        name: tasklist.name,
+      },
+    });
 
-      await documentClient.put({
-        TableName: "mutodo",
-        Item: {
-          id: `user_${authorizedUser.sub}`,
-          sort_key: `tasklist_${uuid}`,
-        },
-      });
-      documentClient.destroy();
-      ddbClient.destroy();
-      return { id: `tasklist_${uuid}` };
-    } catch (error) {
-      documentClient.destroy();
-      ddbClient.destroy();
-      return { id: null };
-    }
-  },
-};
+    await documentClient.put({
+      TableName: "mutodo",
+      Item: {
+        id: `user_${authorizedUser.sub}`,
+        sort_key: `tasklist_${uuid}`,
+      },
+    });
+    documentClient.destroy();
+    ddbClient.destroy();
+    return { id: `tasklist_${uuid}` };
+  } catch (error) {
+    documentClient.destroy();
+    ddbClient.destroy();
+    return { id: null };
+  }
+}
 
-export default tasklistRepository;
+export { create };
